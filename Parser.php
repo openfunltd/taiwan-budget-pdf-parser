0bf6114b34ea42063ca61641955b9f8152d62649
Ronny Wang <ronnywang@gmail.com>
2025-02-16 04:05:42 +0000

    parse more data

--- Parser.php
+++ Parser.php
@@ -27,11 +27,17 @@ class Parser
             $doc = new DOMDocument();
             @$doc->loadHTML($html_content);
             $boxes = [];
+            $prev_box = null;
             foreach ($doc->getElementsByTagName('p') as $p_dom) {
                 $style = $p_dom->getAttribute('style');
                 if (!preg_match('#position:absolute;top:([0-9]+)px;left:([0-9]+)px#', $style, $matches)) {
                     continue;
                 }
+                $current_box = "{$matches[1]}-{$matches[2]}-{$p_dom->textContent}";
+                if ($prev_box == $current_box) {
+                    continue;
+                }
+                $prev_box = $current_box;
                 $boxes[] = [
                     'top' => $matches[1],
                     'left' => $matches[2],
@@ -138,7 +144,7 @@ class Parser
                     if ($black_index === null) {
                         $black_index = count($black);
                     }
-                    $rows[$black_index] = $text;
+                    $rows[$black_index] = str_replace('　', '', $text);
                 }
                 if (!array_key_exists($type, $type_lines)) {
                     $type_lines[$type] = [];
@@ -147,5 +153,143 @@ class Parser
             }
         }
         return $type_lines;
+    }
+
+    public static function outputData($cols, $values)
+    {
+        return array_combine($cols, array_map(function($c) use ($values){
+            return $values[$c] ?? '';
+        }, $cols));
+    }
+
+    public static function parseData($type_lines, $callback)
+    {
+        $cols = [
+            '歲入來源別預算表' => [
+                '款', '項', '目', '節', '編號', '款名', '項名', '目名', '節名', '本年度預算數', '上年度預算數', '前年度決算數', '本年度與上年度比較', '說明',
+            ],
+            '歲出機關別預算表' => [
+                '款', '項', '目', '節', '編號', '款名', '項名', '目名', '節名', '本年度預算數', '上年度預算數', '前年度決算數', '本年度與上年度比較', '說明',
+            ],
+            '歲出政事別預算表' => [
+                '款', '項', '目', '節', '編號', '款名', '項名', '目名', '節名', '本年度預算數', '上年度預算數', '前年度決算數', '本年度與上年度比較',
+            ],
+        ];
+
+        foreach ($type_lines as $type => $type_line) {
+            $no = null;
+            $values = null;
+            $prev_values = [];
+            while ($row = array_shift($type_line)) {
+                if ($row[4] == '合計') {
+                    $values = [
+                        '款名' => '合計',
+                        '本年度預算數' => str_replace(',', '', $row[5]),
+                        '上年度預算數' => str_replace(',', '', $row[6]),
+                        '前年度決算數' => str_replace(',', '', $row[7]),
+                        '本年度與上年度比較' => str_replace(',', '', $row[8]),
+                    ];
+                    continue;
+                }
+
+                // 如果只有第 4 個欄位有值，表示現在是編號
+                if ($row[4] != '') {
+                    $clone_row = $row;
+                    $clone_row[4] = '';
+                    if (implode('', $clone_row) == '') {
+                        if (!preg_match('#^\d+$#', $row[4])) {
+                            throw new Exception("編號不是數字: " . $row[4]);
+                        }
+                        if (!is_null($values)) {
+                            $prev_values = $values;
+                            $callback($type, self::outputData($cols[$type], $values));
+                        }
+                        $values = [
+                            '編號' => $row[4],
+                            '款' => $prev_values['款'] ?? '',
+                            '款名' => $prev_values['款名'] ?? '',
+                            '項' => $prev_values['項'] ?? '',
+                            '項名' => $prev_values['項名'] ?? '',
+                            '目' => $prev_values['目'] ?? '',
+                            '目名' => $prev_values['目名'] ?? '',
+                            '節' => $prev_values['節'] ?? '',
+                            '節名' => $prev_values['節名'] ?? '',
+                            '說明' => '',
+                        ];
+                        continue;
+                    }
+                }
+
+                if ($row[0] != '' and $row[4] != '') {
+                    $values['款'] = $row[0];
+                    $values['款名'] = $row[4];
+                    $values['項'] = '';
+                    $values['項名'] = '';
+                    $values['目'] = '';
+                    $values['目名'] = '';
+                    $values['節'] = '';
+                    $values['節名'] = '';
+                    $row[0] = $row[4] = '';
+                }
+
+                if ($row[1] != '' and $row[4] != '') {
+                    $values['項'] = $row[1];
+                    $values['項名'] = $row[4];
+                    $values['目'] = '';
+                    $values['目名'] = '';
+                    $values['節'] = '';
+                    $values['節名'] = '';
+                    $row[1] = $row[4] = '';
+                }
+
+                if ($row[2] != '' and $row[4] != '') {
+                    $values['目'] = $row[2];
+                    $values['目名'] = $row[4];
+                    $values['節'] = '';
+                    $values['節名'] = '';
+                    $row[2] = $row[4] = '';
+                }
+
+                if ($row[3] != '' and $row[4] != '') {
+                    $values['節'] = $row[3];
+                    $values['節名'] = $row[4];
+                    $row[3] = $row[4] = '';
+                }
+
+                if ($row[5] != '' and $row[6] != '' and $row[7] != '' and $row[8] != '') {
+                    $values['本年度預算數'] = str_replace(',', '', $row[5]);
+                    $values['上年度預算數'] = str_replace(',', '', $row[6]);
+                    $values['前年度決算數'] = str_replace(',', '', $row[7]);
+                    $values['本年度與上年度比較'] = str_replace(',', '', $row[8]);
+                    $row[5] = $row[6] = $row[7] = $row[8] = '';
+                }
+
+                if ($row[9] ?? false and $row[9] != '') {
+                    $values['說明'] .= $row[9];
+                    $row[9] = '';
+                }
+
+                // 如果只剩下 row[4] 有值，有可能是沒有款項目節的資料
+                if ($row[4]) {
+                    $clone_row = $row;
+                    $clone_row[4] = '';
+                    if (implode('', $clone_row) == '') {
+                        $values['目'] = '*';
+                        $values['目名'] = $row[4];
+                        $values['節'] = '';
+                        $values['節名'] = '';
+                        $row[4] = '';
+                    }
+                }
+
+                if (implode('', $row) == '') {
+                    continue;
+                }
+
+                echo "current = " . json_encode($row, JSON_UNESCAPED_UNICODE) . "\n";
+                echo json_encode(array_slice($type_line, 0, 5), JSON_UNESCAPED_UNICODE) . "\n";
+                exit;
+            }
+        }
     }
 }
