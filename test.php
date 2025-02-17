643d9f122cffccf3f4f9f5533b81cbcbb5e41854
Ronny Wang <ronnywang@gmail.com>
2025-02-17 05:13:42 +0000

    add test.php

--- test.php
+++ test.php
@@ -2,8 +2,23 @@
 
 include(__DIR__ . "/Parser.php");
 $file = __DIR__ . "/sample.pdf";
+$file = __DIR__ . '/監察院.pdf';
+$file = __DIR__ . '/原住民族委員會.pdf';
+$file = __DIR__ . '/台北市.pdf';
 $file = __DIR__ . '/內政部.pdf';
-//Parser::toHTML($file);
-$ret = Parser::parseHTML(__DIR__ . "/tmp/tmp-html.html");
-$ret = Parser::parseData($ret);
-print_r($ret);
+if ($_SERVER['argc'] > 1) {
+    $file = $_SERVER['argv'][1];
+}
+Parser::toHTML($file);
+$type_lines = Parser::parseHTML(__DIR__ . "/tmp/tmp-html.html");
+$fps = [];
+$ret = Parser::parseData($type_lines, function($type, $rows) use (&$fps) {
+    if (!($fps[$type] ?? false)) {
+        $fps[$type] = fopen(__DIR__ . "/outputs/{$type}.csv", 'w');
+        fputcsv($fps[$type], array_keys($rows));
+    }
+    fputcsv($fps[$type], array_values($rows));
+});
+foreach($fps AS $fp) {
+    fclose($fp);
+}
